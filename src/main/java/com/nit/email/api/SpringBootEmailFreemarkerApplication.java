3eaa798a6cd724748b6f08bdfca4ace85c053860
TapanTL <TapanTL@gmail.com>
2018-12-01 23:35:16 +0530

    email api

---
+++ src/main/java/com/nit/email/api/SpringBootEmailFreemarkerApplication.java
@@ -0,0 +1,14 @@
+package com.nit.email.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmailFreemarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmailFreemarkerApplication.class, args);
+	}
+}
+
+// use to less access urs security- https://www.google.com/settings/security/lesssecureapps
